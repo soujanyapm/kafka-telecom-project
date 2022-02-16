3f4082219cabd182f82007611469b35bae70b402
Soujanya P M <25240342+soujanyapm@users.noreply.github.com>
2022-02-16 20:22:54 +0530

    Update ksql-connection.py

--- ksql-connection.py
+++ ksql-connection.py
@@ -6,18 +6,17 @@ import requests
 import json
 
 from kafka import KafkaConsumer
-consumer = KafkaConsumer('users')
+consumer = KafkaConsumer('CALL_DATASETS',auto_offset_reset ='earliest')
 for message in consumer:
-    print ((message.key).decode("utf-8") )
-    user = (message.key).decode("utf-8")
-    user_id = int(user.split("_")[1])
+    #print ((message.key).decode("utf-8") )
+    call_data = json.loads((message.value).decode('utf-8')) # decode converts byte to string, json.loads converts stringified dictionary to dictionary
 
     #print(json.loads((message.value).decode("utf-8")))
-    if (user_id <= 5):
-        URL = "<webhook url>"
-        body = {"text":"Hello "+user}
+    if (call_data['CALL_DIRECTION'] == 'Incoming'):
+        URL = "<webhook url>" #slack webhook url
+        body = {"text":"Hello ! It's an incoming call "}
         headers = {"Content-Type":"application/json"}
 
-        result = requests.post(URL, data = json.dumps(body),headers=headers)
+        result = requests.post(URL, data = json.dumps(body),headers=headers) #json.dumps converts python object to json object
 
         print(result)
